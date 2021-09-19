f758556aaa3ab2a876a1bb74ce11923aed64b164
luuniem87 <luuniem87@icloud.com>
2021-09-18 22:09:37 -0700

    onClick recommended title retrieve videotrailer

--- src/components/SeriesMain.jsx
+++ src/components/SeriesMain.jsx
@@ -1,15 +1,34 @@
 import React, { useState } from "react";
 import RecommendedList from "./RecommendedList";
 import Video from "./Video";
+const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SeriesMain = (props) => {
   const [seriesId, setSeriesId] = useState("");
-  console.log(seriesId);
+
+  async function seriesHandler(seriesId) {
+    const videoURL = `https://api.themoviedb.org/3/tv/${seriesId}/videos?api_key=${API_KEY}`;
+
+    const response = await fetch(videoURL);
+    const data = await response.json();
+    const transformToVideo = data.results.map((videoData) => {
+      return {
+        id: videoData.id,
+        youTubeKey: videoData.key,
+        official: videoData.official,
+        trailerType: videoData.type,
+      };
+    });
+    console.log(transformToVideo);
+  }
 
   return (
     <>
       <Video tv_id={seriesId} />
-      <RecommendedList passSeriesId={(seriesId) => setSeriesId(seriesId)} />
+      <RecommendedList
+        //  passSeriesId={(seriesId) => setSeriesId(seriesId)}
+        passSeriesId={seriesHandler}
+      />
     </>
   );
 };
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -1,18 +1,26 @@
 import React, { useState, useCallback, useEffect } from "react";
 import "./SeriesDetailsList.scss";
+import axios from "axios";
 const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SeriesDetailsList = (props) => {
   const { tv_id } = props;
   const [tvVideo, setTvVideo] = useState([]);
-  const [error, setError] = useState(null);
-  const VideoURL = `https://api.themoviedb.org/3/tv/${tv_id}/videos?api_key=${API_KEY}`;
 
-  console.log(tvVideo);
+  async function fetchVideoHandler() {
+    const videoURL = `https://api.themoviedb.org/3/tv/${tv_id}/videos?api_key=${API_KEY}`;
+
+    const response = await fetch(videoURL);
+    const data = await response.json();
+    console.log(data);
+  }
+
   return (
     <>
       <div>
-        {!error && tvVideo.map((videoData) => <div>{videoData.key}</div>)}
+        {/* {tvVideo.map((videoData) => (
+          <div>{videoData.key}</div>
+        ))} */}
       </div>
     </>
   );
