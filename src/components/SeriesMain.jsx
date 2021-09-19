0be4cf6c98d6d5a55085cb50567943ae1bb47fa7
luuniem87 <luuniem87@icloud.com>
2021-09-18 23:17:28 -0700

    set array filter to filter for only official videos

--- src/components/SeriesMain.jsx
+++ src/components/SeriesMain.jsx
@@ -6,29 +6,29 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 const SeriesMain = (props) => {
   const [seriesId, setSeriesId] = useState("");
 
-  async function seriesHandler(seriesId) {
-    const videoURL = `https://api.themoviedb.org/3/tv/${seriesId}/videos?api_key=${API_KEY}`;
+  async function seriesHandler(titleId) {
+    const videoURL = `https://api.themoviedb.org/3/tv/${titleId}/videos?api_key=${API_KEY}`;
 
     const response = await fetch(videoURL);
     const data = await response.json();
-    const transformToVideo = data.results.map((videoData) => {
-      return {
-        id: videoData.id,
-        youTubeKey: videoData.key,
-        official: videoData.official,
-        trailerType: videoData.type,
-      };
-    });
+    const transformToVideo = data.results
+      .filter(({ official }) => official)
+      .map((videoData) => {
+        return {
+          id: videoData.id,
+          youTubeKey: videoData.key,
+          official: videoData.official,
+          trailerType: videoData.type,
+        };
+      });
     console.log(transformToVideo);
+    setSeriesId(transformToVideo);
   }
 
   return (
     <>
-      <Video tv_id={seriesId} />
-      <RecommendedList
-        //  passSeriesId={(seriesId) => setSeriesId(seriesId)}
-        passSeriesId={seriesHandler}
-      />
+      {/* <Video tv_id={seriesId[0].youTubeKey[0]} /> */}
+      <RecommendedList passSeriesId={seriesHandler} />
     </>
   );
 };
--- src/components/Video.jsx
+++ src/components/Video.jsx
@@ -5,23 +5,10 @@ const API_KEY = process.env.REACT_APP_API_KEY;
 
 const SeriesDetailsList = (props) => {
   const { tv_id } = props;
-  const [tvVideo, setTvVideo] = useState([]);
-
-  async function fetchVideoHandler() {
-    const videoURL = `https://api.themoviedb.org/3/tv/${tv_id}/videos?api_key=${API_KEY}`;
-
-    const response = await fetch(videoURL);
-    const data = await response.json();
-    console.log(data);
-  }
 
   return (
     <>
-      <div>
-        {/* {tvVideo.map((videoData) => (
-          <div>{videoData.key}</div>
-        ))} */}
-      </div>
+      <iframe src={"https://www.youtube.com/embed/" + tv_id}></iframe>
     </>
   );
 };
